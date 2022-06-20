729cb1a0b4f4543806db1b3a6c2f8af04cb6c930
Brunadisner <bruna.disner@gmail.com>
2022-06-19 23:05:55 -0300

    tentando inserir os dados do arquivo na tabela

--- trab2BD.py
+++ trab2BD.py
@@ -1,16 +1,21 @@
 import sys
 import re
+import psycopg2
 
-# import psycopg2
-# #função que faz a conexão com o banco de dados
-# host='localhost'
-# dbname='dados'
-# user='postgres'
-# password='postgres'
-# sslmode='require'
-# conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-# conn = psycopg2.connect(conn_string) 
-# cursor = conn.cursor()
+def conexao_BD():
+    #função que faz a conexão com o banco de dados
+    try:
+        conexao = psycopg2.connect(database= 'trabalho',
+                                    host = 'localhost',
+                                    user = 'postgres',
+                                    password = 'root')
+        print("Conectou!")
+        return conexao     
+    except psycopg2.DatabaseError as e:
+        print("Erro ao conectar o banco:", e)
+        return None
+
+con = conexao_BD()
 
 # #função para inserir as tuplas na tabela
 # def inserir_db(sql):
@@ -33,17 +38,58 @@ def main():
     for i in file:
         arquivo_linhas.append(i)
     
-    for string in vetor:
-        for caracter in string:
-                vetor2 = re.findall(pattern,caracter)
-        print(vetor2)
+    arquivo_linhas = limpar(arquivo_linhas)
 
+    for i in range(len(arquivo_linhas)):
+        if arquivo_linhas[i] == '':
+            inserir_banco(arquivo_linhas, i)
+            break
+    
+    
+
+
+
+
+# limpando as linhas para iniciar a leitura do arquivo
 def limpar(arquivo_linhas):
     for linha in range(len(arquivo_linhas)):
-        linha[arquivo_linhas] = re.sub('\n','',arquivo_linhas[linha])
-        linha[arquivo_linhas] = re.sub('<','')
-        linha[arquivo_linhas] = re.sub('>','')
+        arquivo_linhas[linha] = re.sub('\n','',arquivo_linhas[linha])
+        arquivo_linhas[linha] = re.sub('<','',arquivo_linhas[linha])
+        arquivo_linhas[linha] = re.sub('>','',arquivo_linhas[linha])
 
     return arquivo_linhas
 
+
+def inserir_banco(arquivo_linhas,i):
+    cur = con.cursor()
+    cur.execute(sql)
+    inseriu = arquivo_linhas[0:i]
+
+    for arquivo_linhas in inseriu:
+        arquivo_linhas = re.sub('=', ',', arquivo_linhas)
+        quebra = arquivo_linhas.split(',')
+        sql = " select * from tabela1 where id = {}".format(quebra[1])
+        cur.execute(sql)
+        r = cur.fetchall()
+        if r:
+            sql = "update tabela1 set {} = {} where id {}".format(quebra[0], quebra[2], quebra[1])
+            cur.execute(sql)
+        else:
+            sql = "insert into tabela1 (id, {}) values ".format(quebra[0], quebra[1], quebra[2])
+            cur.execute(sql)
+
+    con.commit()
+    imprimir_variaveis()
+
+def imprimir_variaveis():
+    print(" Imprimindo váriaveis ---- ")
+    cur  = con.cursor()
+    sql = "select * from tabela order by id"
+    cur.execute(sql)
+    r = cur.fetchall()
+    print(r)
+    
+
+
+main()
 
